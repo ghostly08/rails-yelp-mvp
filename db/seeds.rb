42a2dc8e7954587798be18fedbcddb23a5cdfaff
ghostly08 <steven.produccionbcn@gmail.com>
2022-02-16 17:18:35 +0100

    adding faker gem and creating seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating 5 restaurants"
+
+5.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    phone_number: Faker::PhoneNumber,
+    category: Restaurant::CATEGORY.sample
+  )
+end
+
+puts "Done!"
