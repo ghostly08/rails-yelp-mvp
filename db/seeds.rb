4180e16037076e95d5d66815c56dee8ff02788f5
ghostly08 <steven.produccionbcn@gmail.com>
2022-02-16 18:41:39 +0100

    creating restaurant controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
 
 puts "Creating 5 restaurants"
 
