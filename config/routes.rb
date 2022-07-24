175a6f1d666099630b3f28ab62c1c5efe144a8d5
ghostly08 <steven.produccionbcn@gmail.com>
2022-07-24 23:03:39 +0200

    traying to add simple form in reviews view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants, except: [ :update, :destroy ] do
     root to: "restaurants#index"
