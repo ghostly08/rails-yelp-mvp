f2d7766b241273379f22c56f359a87a4d0125f71
ghostly08 <steven.produccionbcn@gmail.com>
2022-07-26 09:38:56 +0200

    adding edit restaurant form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -22,18 +22,21 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
 
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
   def destroy
     @restaurant = Restaurant.find(params[:id])
     @restaurant.destroy
     redirect_to restaurants_path, status: :see_other
   end
-
-
-  # def edit
-
-  # end
-
-
 
   private
 
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy #reviews are dependent of the restaurant.
 
-  CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+  CATEGORY = ["chinese", "italian", "japanese", "french", "mediterranean", "asian", "indian", "vegetarian & vegan"]
 
   validates :name, presence: true
   validates :address, presence: true
